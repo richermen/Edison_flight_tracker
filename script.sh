b1434cfd60949bdf028da035c15b13fd511867d0
Rajesh S <rajesh109876@gmail.com>
2016-05-25 14:49:17 +0530

    init.d script to make the device robust

---
+++ script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# /rtlsdr/dump1090/./dump1090 --raw >log.txt
+while  true; do
+echo "System initiating"
+/rtlsdr/dump1090/./dump1090 --raw >log.txt &
+echo "RTL SDR is up and looking for flights"
+for i in `seq 1 15`; do
+	sleep 1
+   	echo -n "."
+done
+killall -9 dump1090
+python final.py 
+done
+
+
+
+#rm log.txt
+
+
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+
+
+function ctrl_c() {
+        echo "** Trapped CTRL-C"
+}
+
+for i in `seq 1 5`; do
+	    sleep 1
+	        echo -n "."
+	done
