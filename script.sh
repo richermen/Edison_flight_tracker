6e2715dd10d976122e5c688fcbe4fb45a0610e1c
Rajesh S <rajesh-s@users.noreply.github.com>
2016-05-25 14:53:54 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@
 # /rtlsdr/dump1090/./dump1090 --raw >log.txt
 while  true; do
 echo "System initiating"
+# raw for log file , interactive for terminal display , help for more options
 /rtlsdr/dump1090/./dump1090 --raw >log.txt &
 echo "RTL SDR is up and looking for flights"
 for i in `seq 1 15`; do
